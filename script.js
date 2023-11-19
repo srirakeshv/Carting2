3fdb5111d6ada462f4c82e0bb7aa27bf6069ffdb
srirakeshv <srirakeshv@gmail.com>
2023-11-19 11:29:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,165 @@
+const array=[
+    {
+        id: 1,
+        image: 'images/download (3).jpeg',
+        name: 'Photo Frame',
+        price: 250,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/downloadsdfs.jpeg',
+        name: 'Keyboard',
+        price: 2500,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (3).jpeg',
+        name: 'Photo Frame',
+        price: 250,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (5).jpeg',
+        name: 'TV',
+        price: 250000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (2).jpeg',
+        name: 'Table',
+        price: 15000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (4).jpeg',
+        name: 'Watch',
+        price: 5000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download.jpeg',
+        name: 'Printer',
+        price: 2500,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (2).jpeg',
+        name: 'Table',
+        price: 25000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (4).jpeg',
+        name: 'Watch',
+        price: 2000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download.jpeg',
+        name: 'Printer',
+        price: 5000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (5).jpeg',
+        name: 'TV',
+        price: 250000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (1).jpeg',
+        name: 'Mobile',
+        price: 25000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (2).jpeg',
+        name: 'Table',
+        price: 15000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (4).jpeg',
+        name: 'Watch',
+        price: 2000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (2).jpeg',
+        name: 'Table',
+        price: 15000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (1).jpeg',
+        name: 'Mobile',
+        price: 25000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/downloadsdfs.jpeg',
+        name: 'Keyboard',
+        price: 2500,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (5).jpeg',
+        name: 'TV',
+        price: 250000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download.jpeg',
+        name: 'Printer',
+        price: 50000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+    {
+        id: 1,
+        image: 'images/download (1).jpeg',
+        name: 'Mobile',
+        price: 25000,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos harum aspernatur iure asperiores impedit velit iusto, iste fugit, non qui, praesentium officiis consequuntur. Reprehenderit itaque velit aspernatur veritatis laborum adipisci tempore perspiciatis, nulla distinctio ipsam ut et aliquam nobis voluptatem enim? Cum, esse beatae! Asperiores dolorum esse amet facere doloremque!',
+    },
+]
+
+let boxes=document.querySelectorAll('.box')
+let divElement=document.querySelector('.product img')
+
+boxes.forEach((box,index)=>{
+    box.querySelector('.box img').src=array[index].image
+    box.querySelector('.box p').innerHTML=array[index].name
+
+    box.addEventListener('click',()=>{
+        const num=index
+        const imageUrl=array[num].image
+        const text=array[num].name
+        const priceTag=array[num].price
+        const desc=array[num].description
+        console.log(imageUrl)
+        localStorage.setItem('img', imageUrl)
+        localStorage.setItem('text', text)
+        localStorage.setItem('price', priceTag)
+        localStorage.setItem('desd', desc)
+        window.location.href='product.html'
+
+    })
+})
